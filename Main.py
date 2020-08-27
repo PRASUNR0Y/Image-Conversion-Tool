103a6b3d1b556f2a3a7ce018c5578426564ea9ce
Prasun Roy <prasunroy212@gmail.com>
2020-08-27 22:44:06 +0530

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image
+
+root = tk.Tk()
+root.title('PNG To JPG Converter')
+
+canv1 = tk.Canvas(root, width=301, height=251, bg='yellow', relief='raised')
+canv1.pack()
+
+label = tk.Label(root, text='File Conversion Tool', bg='green')
+label.config(font=('helvetica', 20))
+canv1.create_window(151, 61, window=label)
+
+
+def getPNGFile():
+    global img1
+    import_file_path = filedialog.askopenfilename()
+    img1 = Image.open(import_file_path)
+
+
+browseButton_PNGFile = tk.Button(text=" Import PNG File ", command=getPNGFile, bg='blue', fg='white',
+                                 font=('helvetica', 12, 'bold'))
+canv1.create_window(151, 131, window=browseButton_PNGFile)
+
+
+def convertToJPGFile():
+    global img1
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg')
+    img1.save(export_file_path)
+
+
+saveAsButton_JPGFile = tk.Button(text='Convert PNG to JPG', command=convertToJPGFile, bg='blue', fg='white',
+                                 font=('helvetica', 12, 'bold'))
+canv1.create_window(151, 181, window=saveAsButton_JPGFile)
+
+root.mainloop()
