ade3e096a0dd84ae0ca3ef4acece8b2396846c5e
Prasun Roy <prasunroy212@gmail.com>
2020-08-28 11:44:17 +0530

    Fixed RGB bug

--- Main.py
+++ Main.py
@@ -15,9 +15,10 @@ canv1.create_window(151, 61, window=label)
 
 def getPNGFile():
     global img1
+    global rgb
     import_file_path = filedialog.askopenfilename()
     img1 = Image.open(import_file_path)
-
+    rgb = img1.convert('RGB')
 
 browseButton_PNGFile = tk.Button(text=" Import PNG File ", command=getPNGFile, bg='blue', fg='white',
                                  font=('helvetica', 12, 'bold'))
@@ -26,8 +27,9 @@ canv1.create_window(151, 131, window=browseButton_PNGFile)
 
 def convertToJPGFile():
     global img1
+    global rgb
     export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg')
-    img1.save(export_file_path)
+    rgb.save(export_file_path)
 
 
 saveAsButton_JPGFile = tk.Button(text='Convert PNG to JPG', command=convertToJPGFile, bg='blue', fg='white',
