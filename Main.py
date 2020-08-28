2df5d9d3f5e1cb98d150e537b9fb11bda536e81b
Prasun Roy <prasunroy212@gmail.com>
2020-08-28 12:11:51 +0530

    Add JPG to PNG feature

--- Main.py
+++ Main.py
@@ -3,24 +3,23 @@ from tkinter import filedialog
 from PIL import Image
 
 root = tk.Tk()
-root.title('PNG To JPG Converter')
+root.title('Image Conversion Tool')
 
 canv1 = tk.Canvas(root, width=301, height=251, bg='yellow', relief='raised')
 canv1.pack()
 
-label = tk.Label(root, text='File Conversion Tool', bg='green')
+label = tk.Label(root, text='Image Conversion Tool', bg='green')
 label.config(font=('helvetica', 20))
 canv1.create_window(151, 61, window=label)
 
 
 def getPNGFile():
     global img1
-    global rgb
     import_file_path = filedialog.askopenfilename()
     img1 = Image.open(import_file_path)
-    rgb = img1.convert('RGB')
 
-browseButton_PNGFile = tk.Button(text=" Import PNG File ", command=getPNGFile, bg='blue', fg='white',
+
+browseButton_PNGFile = tk.Button(text=" Import File ", command=getPNGFile, bg='blue', fg='white',
                                  font=('helvetica', 12, 'bold'))
 canv1.create_window(151, 131, window=browseButton_PNGFile)
 
@@ -29,11 +28,24 @@ def convertToJPGFile():
     global img1
     global rgb
     export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg')
+    rgb = img1.convert('RGB')
     rgb.save(export_file_path)
 
 
-saveAsButton_JPGFile = tk.Button(text='Convert PNG to JPG', command=convertToJPGFile, bg='blue', fg='white',
+def convertToPNGFile():
+    global img1
+    global rgba
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.png')
+    rgba = img1.convert('RGBA')
+    rgba.save(export_file_path)
+
+
+saveAsButton_JPGFile = tk.Button(text='Convert to JPG & Save', command=convertToJPGFile, bg='blue', fg='white',
                                  font=('helvetica', 12, 'bold'))
 canv1.create_window(151, 181, window=saveAsButton_JPGFile)
+
+saveAsButton_PNGFile = tk.Button(text='Convert to PNG & Save', command=convertToPNGFile, bg='blue', fg='white',
+                                 font=('helvetica', 12, 'bold'))
+canv1.create_window(151, 220, window=saveAsButton_PNGFile)
 
 root.mainloop()
